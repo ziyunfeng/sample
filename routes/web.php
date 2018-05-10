54fb4645348a3adb8198d9a9100158ccc71faee6
Jean <245908530@qq.com>
2018-05-10 08:48:19 +0000

    activation-password reset

--- routes/web.php
+++ routes/web.php
@@ -26,3 +26,11 @@ Route::resource('user', "UserController");
 Route::get('login', 'SessionsController@create')->name('login');    //  显示登录页
 Route::post('login', 'SessionsController@store')->name('login');   //  创建新回话（登录）
 Route::delete('logout', "SessionsController@destroy")->name('logout'); //  销毁登录
+
+//  密码重置
+
+Route::get('password/reset', "Auth\ForgotPasswordController@showLinkRequestForm")->name('password.request');
+Route::post('password/email','Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
+
+Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
+Route::post('password/reset', 'Auth\ResetPasswordController@reset')->name('password.update');
